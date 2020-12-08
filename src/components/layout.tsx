b6e097d69f093a8516834cb1c0d4daaf80c3b085
T. Kameyama <tkamenoko@vivaldi.net>
2020-12-08 15:12:53 +0900

    add: basic page

---
+++ src/components/layout.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import { NextSeo } from 'next-seo';
+import { PropsWithChildren, ReactElement } from 'react';
+
+import styles from './layout.module.scss';
+
+export default function Layout({
+  title,
+  children,
+}: PropsWithChildren<{ title: string }>): ReactElement {
+  return (
+    <div className={styles.container}>
+      <NextSeo titleTemplate="%s | Your Site Name" title={title} />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <main className={styles.app}>{children}</main>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+import Layout from '@/components/layout';
+
+export default function Index(): ReactElement {
+  return (
+    <Layout title="example">
+      <h1>This is an example</h1>
+    </Layout>
+  );
+}
